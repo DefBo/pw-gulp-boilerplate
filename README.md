bcb4377ab9b6cc5b74e206e1c8bc23ed92f6202b
boandriiv <bo.andriiv@gmail.com>
2020-11-28 12:14:19 +0200

    Initial build set-up

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# pw-gulp-boilerplate
+# Welcome to ProcessWire 3.x with GULP integration
+
+Go to gulpfile.js and update line 60 with your local server domain
+`proxy: "centurion:8888", // replace with your local server domain (f.e - localhost:8080)`
+
+1. `npm i` (in root) - install dependencies
+2. `gulp` - run build
+
---
+++ gulpfile.js
@@ -0,0 +1,125 @@
+// VARIABLES & PATHS
+
+let preprocessor = 'scss', // Preprocessor (sass, scss, less, styl)
+    fileswatch   = 'php,html,htm,txt,json,md,woff2', // List of files extensions for watching & hard reload (comma separated)
+    imageswatch  = 'jpg,jpeg,png,webp,svg', // List of images extensions for watching & compression (comma separated)
+    baseDir      = 'site', // (app) Base directory path without «/» at the end
+    online       = true; // If «false» - Browsersync will work offline without internet connection
+
+let paths = {
+
+	scripts: {
+		src: [
+			// 'node_modules/jquery/dist/jquery.min.js', // npm vendor example (npm i --save-dev jquery)
+			baseDir + '/templates/scripts/main.js' // app.js. Always at the end
+		],
+		dest: baseDir + '/templates/scripts',
+	},
+
+	styles: {
+		src:  baseDir + '/templates/' + preprocessor + '/main.*',
+		dest: baseDir + '/templates/styles',
+	},
+
+	images: {
+		src:  baseDir + '/assets/files/images/src/**/*',
+		dest: baseDir + '/assets/files/images/dest',
+	},
+
+	deploy: {
+		hostname:    'username@yousite.com', // Deploy hostname
+		destination: 'yousite/public_html/', // Deploy destination
+		include:     [/* '*.htaccess' */], // Included files to deploy
+		exclude:     [ '**/Thumbs.db', '**/*.DS_Store' ], // Excluded files from deploy
+	},
+
+	cssOutputName: 'main.css',
+	jsOutputName:  'main.js',
+
+}
+
+// LOGIC
+
+const { src, dest, parallel, series, watch } = require('gulp');
+const sass         = require('gulp-sass');
+const scss         = require('gulp-sass');
+const less         = require('gulp-less');
+const styl         = require('gulp-stylus');
+const cleancss     = require('gulp-clean-css');
+const concat       = require('gulp-concat');
+const browserSync  = require('browser-sync').create();
+const uglify       = require('gulp-uglify-es').default;
+const autoprefixer = require('gulp-autoprefixer');
+const imagemin     = require('gulp-imagemin');
+const newer        = require('gulp-newer');
+const rsync        = require('gulp-rsync');
+const del          = require('del');
+
+function browsersync() {
+	browserSync.init({
+		proxy: "centurion:8888", // replace with your local server domain (f.e - localhost:8080)
+		notify: false,
+		online: online,
+	})
+}
+
+function scripts() {
+	return src(paths.scripts.src)
+	.pipe(concat(paths.jsOutputName))
+	//.pipe(uglify())
+	.pipe(dest(paths.scripts.dest))
+	.pipe(browserSync.stream())
+}
+
+function styles() {
+	return src(paths.styles.src)
+	.pipe(eval(preprocessor)())
+	.pipe(concat(paths.cssOutputName))
+	.pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true }))
+	.pipe(cleancss( {level: { 1: { specialComments: 0 } },/* format: 'beautify' */ }))
+	.pipe(dest(paths.styles.dest))
+	.pipe(browserSync.stream())
+}
+
+function images() {
+	return src(paths.images.src)
+	.pipe(newer(paths.images.dest))
+	.pipe(imagemin())
+	.pipe(dest(paths.images.dest))
+}
+
+function cleanimg() {
+	return del('' + paths.images.dest + '/**/*', { force: true })
+}
+
+function deploy() {
+	return src(baseDir + '/')
+	.pipe(rsync({
+		root: baseDir + '/',
+		hostname: paths.deploy.hostname,
+		destination: paths.deploy.destination,
+		include: paths.deploy.include,
+		exclude: paths.deploy.exclude,
+		recursive: true,
+		archive: true,
+		silent: false,
+		compress: true
+	}))
+}
+
+function startwatch() {
+	watch(baseDir  + '/templates/' + preprocessor + '/**/*', {usePolling: true}, styles);
+	watch(baseDir  + '/assets/files/images/src/**/*.{' + imageswatch + '}', {usePolling: true}, images);
+	watch(baseDir  + '/**/*.{' + fileswatch + '}', {usePolling: true}).on('change', browserSync.reload);
+	watch([baseDir + '/js/**/*.js', '!' + paths.scripts.dest + '/*.min.js'], {usePolling: true}, scripts);
+}
+
+exports.browsersync = browsersync;
+exports.assets      = series(cleanimg, styles, scripts, images);
+exports.styles      = styles;
+exports.scripts     = scripts;
+exports.images      = images;
+exports.cleanimg    = cleanimg;
+exports.deploy      = deploy;
+exports.dev         = parallel(images, styles, scripts, startwatch);
+exports.default     = parallel(images, styles, scripts, browsersync, startwatch);
